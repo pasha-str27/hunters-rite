6344842ead6a57e4a9c770b9180ac7a7f7399310
Andriy Podobailo <47588502+EndL11@users.noreply.github.com>
2021-04-15 14:30:11 +0300

    project setted

--- godot_game_STUDIO_project/godot_game_STUDIO_project/texture.cpp
+++ godot_game_STUDIO_project/godot_game_STUDIO_project/texture.cpp
@@ -3,6 +3,7 @@
 void godot::texture::_register_methods() 
 {
 	register_method((char*)"_ready", &texture::_ready);
+	register_property<texture, Ref<PackedScene>>("prefab", &texture::spritePrefab, nullptr);
 }
 
 void godot::texture::_init() {}
@@ -10,11 +11,18 @@ void godot::texture::_init() {}
 void godot::texture::_ready()
 {
 	Godot::print("jdjdf");
-	set_flip_v(true);
+	
+	auto tt = ResourceLoader::get_singleton()->load("res://skull.png");
+	set_texture(tt);
+	//set_flip_v(true);
+
+	//add_child(cast_to<Sprite>(spritePrefab->instance()));
+
 }
 
 void godot::texture::_process(float delta)
 {
+	//add_child(cast_to<Sprite>(spritePrefab->instance()));
 }
 
 godot::texture::~texture()
@@ -23,4 +31,5 @@ godot::texture::~texture()
 
 godot::texture::texture()
 {
+	spritePrefab = nullptr;
 }
