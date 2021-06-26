edf1a8c2c3ce5a3fa626b7b0d1a3bbc39df96382
Vitalii Boiko <boiko_ak18@nuwm.edu.ua>
2021-06-26 15:48:11 +0300

    Added resources system to  MiniMapController and testing adding room icon as child to grid with fixed position

--- godot_game_STUDIO_project/godot_game_STUDIO_project/MiniMapController.cpp
+++ godot_game_STUDIO_project/godot_game_STUDIO_project/MiniMapController.cpp
@@ -21,9 +21,9 @@ void godot::MiniMapController::_register_methods()
 	register_method("_init", &MiniMapController::_init);
 	register_method("_ready", &MiniMapController::_ready);
 
-	register_property<MiniMapController, Texture*>("current_room", &MiniMapController::curr_room, nullptr);
-	register_property<MiniMapController, Sprite*>("discovered_room", &MiniMapController::disc_room, nullptr);
-	register_property<MiniMapController, Sprite*>("undiscovered_room", &MiniMapController::undisc_room, nullptr);
+	register_property<MiniMapController, Ref<PackedScene>>("current_room", &MiniMapController::curr_room, nullptr);
+	register_property<MiniMapController, Ref<PackedScene>>("discovered_room", &MiniMapController::disc_room, nullptr);
+	register_property<MiniMapController, Ref<PackedScene>>("undiscovered_room", &MiniMapController::undisc_room, nullptr);
 	register_property<MiniMapController, float>("zoom", &MiniMapController::zoom, 1.5f);
 }
 
@@ -33,28 +33,70 @@ void godot::MiniMapController::_init()
 
 void godot::MiniMapController::_ready()
 {
+	if (!_loadResources())
+		Godot::print("Something went wrong! Can not to load resources");
+
 	grid = cast_to<TextureRect>(find_node("Grid"));
 
 	if (grid != nullptr)
-		Godot::print("now we have grid");
-
-	//rooms_positions = LevelGenerator::_get_rooms_positions();
-
-	int offset = 16;
-
-	rooms_positions = { Vector2(0, 0), Vector2(1, 0), Vector2(0, -1) };
-
-	for (auto pos : rooms_positions)
 	{
-		Godot::print(pos);
-		auto sp = Sprite::_new();
-		sp->set_texture(curr_room);
-		grid->add_child(sp);
+		Godot::print("Grid loaded");
+
+		//rooms_positions = LevelGenerator::_get_rooms_positions();
+
+		grid_rect_size = grid->get_rect().get_size();
+
+		curr_room_pos = grid_rect_size / 2;
+		grid_scale = grid_rect_size / (get_viewport_rect().size * zoom);
 
 	}
+
+	rooms_positions = { Vector2(step_x, step_y), Vector2(0, step_y), Vector2(0, 0) };
+
+	Godot::print("Rooms pos setted");
+
+	std::vector<Vector2> fixed_rooms_positions;
+
+	for (auto room : rooms_positions)
+	{
+		auto room_pos = (room - curr_room_pos) * grid_scale + grid_rect_size / 2;
+		fixed_rooms_positions.push_back(room_pos);
+	}
+
+	Godot::print("Rooms pos fixed");
+
+	for (auto pos : fixed_rooms_positions)
+	{
+		Godot::print(pos);
+
+		auto room = cast_to<Node2D>(undisc_room->instance());
+		room->set_position(pos);
+
+		grid->add_child(room);
+
+		Godot::print("Added sprite as child to grid");
+	}
+
+	Godot::print("Added sprites");
 
 }
 
 void godot::MiniMapController::_process()
 {
+}
+
+bool godot::MiniMapController::_loadResources()
+{
+	ResourceLoader* rld = ResourceLoader::get_singleton();
+
+	if (curr_room == nullptr)
+		curr_room = rld->load("res://Assets/Prefabs/MiniMapUi/curr_room.tscn");
+	if (disc_room == nullptr)
+		disc_room = rld->load("res://Assets/Prefabs/MiniMapUi/disc_room.tscn");
+	if (undisc_room == nullptr)
+		undisc_room = rld->load("res://Assets/Prefabs/MiniMapUi/undisc_room.tscn");
+
+	rld = nullptr;
+
+	return true;
 }
--- godot_game_STUDIO_project/godot_game_STUDIO_project/MiniMapController.h
+++ godot_game_STUDIO_project/godot_game_STUDIO_project/MiniMapController.h
@@ -11,9 +11,9 @@ namespace godot
 	private:
 		GODOT_CLASS(MiniMapController, MarginContainer);
 
-		Texture* curr_room = nullptr;
-		Sprite* undisc_room = nullptr;
-		Sprite* disc_room = nullptr;
+		Ref<PackedScene> curr_room = nullptr;
+		Ref<PackedScene> undisc_room = nullptr;
+		Ref<PackedScene> disc_room = nullptr;
 
 		TextureRect* grid = nullptr;
 
@@ -23,13 +23,17 @@ namespace godot
 		float step_x = 1024;
 		float step_y = 720;
 
+		Vector2 grid_scale = Vector2::ZERO;
+		Vector2 curr_room_pos = Vector2::ZERO;
+		Vector2 grid_rect_size = Vector2::ZERO;
+
 	public:
 		static void _register_methods();
 		void _init();
 		void _ready();
 		void _process();
 
-		
+		bool _loadResources();
 
 		MiniMapController();
 		~MiniMapController();
