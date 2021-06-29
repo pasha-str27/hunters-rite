3769d5235e9f5558b1ee482146529b6f89ec7e10
Vitalii Boiko <boiko_ak18@nuwm.edu.ua>
2021-06-29 10:51:48 +0300

    Added functions for rooms loading

--- godot_game_STUDIO_project/godot_game_STUDIO_project/MiniMapController.cpp
+++ godot_game_STUDIO_project/godot_game_STUDIO_project/MiniMapController.cpp
@@ -15,6 +15,7 @@ godot::MiniMapController::~MiniMapController()
 
 	grid = nullptr;
 	all_rooms.clear();
+	disc_rooms_positions.clear();
 }
 
 void godot::MiniMapController::_register_methods()
@@ -27,6 +28,10 @@ void godot::MiniMapController::_register_methods()
 	register_method("_normalize_room_pos", &MiniMapController::_normalize_room_pos);
 	register_method("_clear_map()", &MiniMapController::_clear_map);
 	register_method("_normalize_all_rooms", &MiniMapController::_normalize_all_rooms);
+	register_method("_load_disc_rooms", &MiniMapController::_load_disc_rooms);
+	register_method("_load_curr_room", &MiniMapController::_load_curr_room);
+	register_method("_load_undisc_rooms", &MiniMapController::_load_undisc_rooms);
+	register_method("_is_on_grid", &MiniMapController::_is_on_grid);
 	
 	register_property<MiniMapController, Ref<PackedScene>>("current_room", &MiniMapController::curr_room, nullptr);
 	register_property<MiniMapController, Ref<PackedScene>>("discovered_room", &MiniMapController::disc_room, nullptr);
@@ -41,7 +46,7 @@ void godot::MiniMapController::_init()
 void godot::MiniMapController::_ready()
 {
 	if (!_load_resources())
-		Godot::print("Something went wrong! Can not to load resources");
+		Godot::print("Something went wrong! Resources weren't loaded");
 
 	grid = cast_to<TextureRect>(find_node("Grid"));
 
@@ -53,15 +58,16 @@ void godot::MiniMapController::_ready()
 
 		grid_rect_size = grid->get_rect().get_size();
 
-		curr_room_marker_pos = grid_rect_size / 2;
+		curr_room_position = grid_rect_size / 2;
 		grid_scale = grid_rect_size / (get_viewport_rect().size * zoom);
 	}
 	else
 	{
-		Godot::print("Can not load grid");
+		Godot::print("Grid wasn't loaded");
 		return;
 	}
 
+	//test pos
 	rooms_positions =
 	{
 		Vector2(0, 0),
@@ -82,11 +88,11 @@ void godot::MiniMapController::_ready()
 
 	Godot::print("Rooms pos fixed: ");
 
-	for (auto pos : _normalize_all_rooms())
+	/*for (auto pos : _normalize_all_rooms(rooms_positions))
 	{
 		Godot::print(pos);
 
-		if (grid->get_rect().has_point(pos + grid->get_rect().position))
+		if (_is_on_grid(pos))
 		{
 			auto room = cast_to<Node2D>(undisc_room->instance());
 			room->set_position(pos);
@@ -96,8 +102,7 @@ void godot::MiniMapController::_ready()
 		else
 			Godot::print("Pos of room is out of range");
 	}
-	Godot::print("Added sprites");
-
+	Godot::print("Added sprites");*/
 	all_rooms = grid->get_children();
 
 	_update_minimap();
@@ -127,23 +132,37 @@ void godot::MiniMapController::_update_minimap()
 {
 	Node2D* room = nullptr;
 
-	for (int i = 0; i < all_rooms.size(); ++i)
+	if (all_rooms.size() > 0)
 	{
-		room = cast_to<Node2D>(all_rooms[i]);
-
-		if (rooms_positions[i] == players_pos)
+		for (int i = 0; i < all_rooms.size(); ++i)
 		{
-			room->queue_free();
-			auto room = cast_to<Node2D>(curr_room->instance());
-			room->set_position(_normalize_room_pos(players_pos));
-			room->set_scale(Vector2(4, 4));
-			grid->add_child(room);
+			room = cast_to<Node2D>(all_rooms[i]);
+
+			//need func for this
+			if (rooms_positions[i] == players_pos)
+			{
+				_clear_map();
+
+				_load_disc_rooms(disc_rooms_positions);
+
+				disc_rooms_positions.push_back(rooms_positions[i]);
+				rooms_positions.erase(rooms_positions.begin() + i);
+
+				_load_curr_room(players_pos);
+				_load_undisc_rooms(rooms_positions);
+			}
 		}
+	}
+	else
+	{
+		_load_undisc_rooms(rooms_positions);
+		Godot::print("Rooms were loaded by new function");
 	}
 }
 
 Vector2 godot::MiniMapController::_get_players_pos()
 {
+	//just random numbers for test
 	Ref<RandomNumberGenerator> rng = RandomNumberGenerator::_new();
 	rng->randomize();
 
@@ -171,15 +190,52 @@ void godot::MiniMapController::_clear_map()
 		grid->get_child(i)->queue_free();
 }
 
-std::vector<Vector2> godot::MiniMapController::_normalize_all_rooms()
+std::vector<Vector2> godot::MiniMapController::_normalize_all_rooms(std::vector<Vector2> rooms_pos_array)
 {
 	std::vector<Vector2> fixed_rooms_positions;
 
-	for (auto old_pos : rooms_positions)
+	for (auto old_pos : rooms_pos_array)
 	{
 		auto room_pos = _normalize_room_pos(old_pos);
 		fixed_rooms_positions.push_back(room_pos);
 	}
 
 	return fixed_rooms_positions;
+}
+
+void godot::MiniMapController::_load_disc_rooms(std::vector<Vector2> disc_rooms_pos)
+{
+	for (auto pos : _normalize_all_rooms(disc_rooms_pos))
+		if (_is_on_grid(pos))
+		{
+			auto room = cast_to<Node2D>(disc_room->instance());
+			room->set_position(pos);
+			room->set_scale(Vector2(4, 4));
+			grid->add_child(room);
+		}
+}
+
+void godot::MiniMapController::_load_curr_room(Vector2 curr_room_pos)
+{
+	auto room = cast_to<Node2D>(curr_room->instance());
+	room->set_position(_normalize_room_pos(curr_room_pos));
+	room->set_scale(Vector2(4, 4));
+	grid->add_child(room);
+}
+
+void godot::MiniMapController::_load_undisc_rooms(std::vector<Vector2> undisc_rooms_pos)
+{
+	for (auto pos : _normalize_all_rooms(undisc_rooms_pos))
+		if (_is_on_grid(pos))
+		{
+			auto room = cast_to<Node2D>(undisc_room->instance());
+			room->set_position(pos);
+			room->set_scale(Vector2(4, 4));
+			grid->add_child(room);
+		}
+}
+
+bool godot::MiniMapController::_is_on_grid(Vector2 pos)
+{
+	return grid->get_rect().has_point(pos + grid->get_rect().position);
 }
--- godot_game_STUDIO_project/godot_game_STUDIO_project/MiniMapController.h
+++ godot_game_STUDIO_project/godot_game_STUDIO_project/MiniMapController.h
@@ -19,13 +19,13 @@ namespace godot
 
 		std::vector<Vector2> rooms_positions;
 		std::vector<Vector2> disc_rooms_positions;
+		Vector2 curr_room_position;
 
 		float zoom = 1.5;
 		float step_x = 1024;
 		float step_y = 720;
 
 		Vector2 grid_scale = Vector2::ZERO;
-		Vector2 curr_room_marker_pos = Vector2::ZERO;
 		Vector2 grid_rect_size = Vector2::ZERO;
 		Vector2 players_pos = Vector2::ZERO;
 
@@ -42,7 +42,11 @@ namespace godot
 		Vector2 _get_players_pos();
 		Vector2 _normalize_room_pos(Vector2 old_pos);
 		void _clear_map();
-		std::vector<Vector2> _normalize_all_rooms();
+		std::vector<Vector2> _normalize_all_rooms(std::vector<Vector2> rooms_pos_array);
+		void _load_disc_rooms(std::vector<Vector2> disc_rooms_pos);
+		void _load_curr_room(Vector2 curr_room_pos);
+		void _load_undisc_rooms(std::vector<Vector2> undisc_rooms_pos);
+		bool _is_on_grid(Vector2 pos);
 
 		MiniMapController();
 		~MiniMapController();
