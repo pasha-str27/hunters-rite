176e6032c7c93b61f80b8da71bbf2b084757cebc
Vitalii Boiko <boiko_ak18@nuwm.edu.ua>
2021-07-01 23:22:45 +0300

    Added boss-, item-, key-, heal- room types to minimap

--- godot_game_STUDIO_project/godot_game_STUDIO_project/MiniMapController.cpp
+++ godot_game_STUDIO_project/godot_game_STUDIO_project/MiniMapController.cpp
@@ -12,10 +12,20 @@ godot::MiniMapController::~MiniMapController()
 	curr_room = nullptr;
 	undisc_room = nullptr;
 	disc_room = nullptr;
+	key_room = nullptr;
+	heal_room = nullptr;
+	item_room = nullptr; 
+	boss_room = nullptr;
 
 	grid = nullptr;
+	timer = nullptr;
+
 	disc_rooms_positions.clear();
 	undisc_rooms_positions.clear();
+	keys_positions.clear();
+	items_positions.clear();
+	heal_positions.clear();
+	boss_positions.clear();
 }
 
 void godot::MiniMapController::_register_methods()
@@ -30,10 +40,12 @@ void godot::MiniMapController::_register_methods()
 	register_method("_normalize_room_pos", &MiniMapController::_normalize_room_pos);
 	register_method("_clear_map", &MiniMapController::_clear_map);
 	register_method("_normalize_all_rooms", &MiniMapController::_normalize_all_rooms);
-	register_method("_load_disc_rooms", &MiniMapController::_load_disc_rooms);
 	register_method("_load_curr_room", &MiniMapController::_load_curr_room);
 	register_method("_load_undisc_rooms", &MiniMapController::_load_undisc_rooms);
 	register_method("_is_on_grid", &MiniMapController::_is_on_grid);
+	register_method("_load_special_rooms", &MiniMapController::_load_special_rooms);
+	register_method(" _special_rooms_loader", &MiniMapController::_special_rooms_loader);
+	register_method("_update_special_rooms", &MiniMapController::_update_special_rooms);
 
 	register_method("_start_timer", &MiniMapController::_start_timer);
 	register_method("_on_timeout", &MiniMapController::_on_timeout);
@@ -41,6 +53,10 @@ void godot::MiniMapController::_register_methods()
 	register_property<MiniMapController, Ref<PackedScene>>("current_room", &MiniMapController::curr_room, nullptr);
 	register_property<MiniMapController, Ref<PackedScene>>("discovered_room", &MiniMapController::disc_room, nullptr);
 	register_property<MiniMapController, Ref<PackedScene>>("undiscovered_room", &MiniMapController::undisc_room, nullptr);
+	register_property<MiniMapController, Ref<PackedScene>>("key_room", &MiniMapController::key_room, nullptr);
+	register_property<MiniMapController, Ref<PackedScene>>("item_room", &MiniMapController::item_room, nullptr);
+	register_property<MiniMapController, Ref<PackedScene>>("heal_room", &MiniMapController::heal_room, nullptr);
+	register_property<MiniMapController, Ref<PackedScene>>("boss_room", &MiniMapController::boss_room, nullptr);
 	register_property<MiniMapController, float>("zoom", &MiniMapController::zoom, 1.5f);
 }
 
@@ -63,6 +79,15 @@ bool godot::MiniMapController::_load_resources()
 		disc_room = rld->load("res://Assets/Prefabs/MiniMapUi/disc_room.tscn");
 	if (undisc_room == nullptr)
 		undisc_room = rld->load("res://Assets/Prefabs/MiniMapUi/undisc_room.tscn");
+	if (key_room == nullptr)
+		undisc_room = rld->load("res://Assets/Prefabs/MiniMapUi/key_room.tscn");
+	if (item_room == nullptr)
+		undisc_room = rld->load("res://Assets/Prefabs/MiniMapUi/item_room.tscn");
+	if (heal_room == nullptr)
+		undisc_room = rld->load("res://Assets/Prefabs/MiniMapUi/heal_room.tscn");
+	if (boss_room == nullptr)
+		undisc_room = rld->load("res://Assets/Prefabs/MiniMapUi/boss_room.tscn");
+
 
 	grid = cast_to<TextureRect>(find_node("Grid"));
 
@@ -75,14 +100,12 @@ bool godot::MiniMapController::_load_resources()
 		undisc_rooms_positions = get_node("/root/Node2D/Node/Generation")->call("_get_rooms_positions");
 		undisc_rooms_positions = undisc_rooms_positions[0];
 
+		_set_positions();
+
 		Godot::print(undisc_rooms_positions.size());
 
 		curr_room_position = grid_rect_size / 2;
 		grid_scale = grid_rect_size / (get_viewport_rect().size * zoom);
-		Godot::print("grid_scale");
-		Godot::print(grid_scale);
-		Godot::print("grid_rect_size");
-		Godot::print(grid_rect_size);
 	}
 	else
 	{
@@ -115,34 +138,42 @@ void godot::MiniMapController::_update_minimap()
 	Godot::print("Getting players pos");
 	players_pos = _get_players_pos();
 
-	if (undisc_rooms_positions.size() > 0 || disc_rooms_positions.size() > 0)
+	if (undisc_rooms_positions.size() > 0
+		|| disc_rooms_positions.size() > 0
+		|| key_rooms_positions.size() > 0
+		|| item_rooms_positions.size() > 0
+		|| heal_rooms_positions.size() > 0
+		|| boss_rooms_positions.size() > 0)
 	{
 		if (undisc_rooms_positions.find(players_pos) != -1)
 		{
 			_clear_map();
 			_load_curr_room(players_pos);
-			_load_disc_rooms(disc_rooms_positions);
+			_special_rooms_loader(disc_rooms_positions,
+				key_rooms_positions,
+				item_rooms_positions,
+				heal_rooms_positions,
+				boss_rooms_positions);
 
-			disc_rooms_positions.push_back(players_pos);
+			if (keys_positions.find(players_pos) != -1)
+				key_rooms_positions.push_back(players_pos);
+			else if (items_positions.find(players_pos) != -1)
+				item_rooms_positions.push_back(players_pos);
+			else if (heal_positions.find(players_pos) != -1)
+				heal_rooms_positions.push_back(players_pos);
+			else if (boss_positions.find(players_pos) != -1)
+				boss_rooms_positions.push_back(players_pos);
+			else
+				disc_rooms_positions.push_back(players_pos);
+
 			undisc_rooms_positions.remove(undisc_rooms_positions.find(players_pos));
 
 			_load_undisc_rooms(undisc_rooms_positions);
 
 			return;
 		}
-		if (disc_rooms_positions.find(players_pos) != -1)
-		{
-			_clear_map();
-			_load_curr_room(players_pos);
-
-			disc_rooms_positions.remove(disc_rooms_positions.find(players_pos));
-			_load_disc_rooms(disc_rooms_positions);
-			disc_rooms_positions.push_back(players_pos);
-
-			_load_undisc_rooms(undisc_rooms_positions);
-
-			return;
-		}
+		else
+			_update_special_rooms();
 		Godot::print("Players pos is out of range");
 	}
 	else
@@ -185,21 +216,6 @@ Array godot::MiniMapController::_normalize_all_rooms(Array rooms_pos_array)
 	return fixed_rooms_positions;
 }
 
-void godot::MiniMapController::_load_disc_rooms(Array disc_rooms_pos)
-{
-	for (int i = 0; i < disc_rooms_pos.size(); ++i)
-	{
-		Vector2 pos = _normalize_all_rooms(disc_rooms_pos)[i];
-		if (_is_on_grid(pos))
-		{
-			auto room = cast_to<Node2D>(disc_room->instance());
-			room->set_position(pos);
-			room->set_scale(room_icon_scale);
-			grid->add_child(room);
-		}
-	}
-}
-
 void godot::MiniMapController::_load_curr_room(Vector2 curr_room_pos)
 {
 	auto room = cast_to<Node2D>(curr_room->instance());
@@ -221,6 +237,84 @@ void godot::MiniMapController::_load_undisc_rooms(Array undisc_rooms_pos)
 			grid->add_child(room);
 		}
 	}
+}
+
+void godot::MiniMapController::_load_special_rooms(Array rooms_pos, String type)
+{
+	for (int i = 0; i < rooms_pos.size(); ++i)
+	{
+		Vector2 pos = _normalize_all_rooms(rooms_pos)[i];
+		if (_is_on_grid(pos))
+		{
+			Node2D* room = nullptr;
+
+			if (type == "boss")
+				room = cast_to<Node2D>(boss_room->instance());
+			else if (type == "key")
+				room = cast_to<Node2D>(key_room->instance());
+			else if (type == "item")
+				room = cast_to<Node2D>(item_room->instance());
+			else if (type == "heal")
+				room = cast_to<Node2D>(heal_room->instance());
+			else
+				room = cast_to<Node2D>(disc_room->instance());
+		
+			room->set_position(pos);
+			room->set_scale(room_icon_scale);
+			grid->add_child(room);
+		}
+	}
+}
+
+void godot::MiniMapController::_special_rooms_loader(Array dr, Array kr, Array ir, Array hr, Array br)
+{
+	_load_special_rooms(dr, "disc");
+	_load_special_rooms(kr, "key");
+	_load_special_rooms(ir, "item");
+	_load_special_rooms(hr, "heal");
+	_load_special_rooms(br, "boss");
+
+}
+
+void godot::MiniMapController::_update_special_rooms()
+{
+	_clear_map();
+	_load_curr_room(players_pos);
+
+	if (disc_rooms_positions.find(players_pos) != -1)
+	{
+		disc_rooms_positions.remove(disc_rooms_positions.find(players_pos));
+		_special_rooms_loader(disc_rooms_positions, key_rooms_positions, item_rooms_positions, heal_rooms_positions, boss_rooms_positions);
+		disc_rooms_positions.push_back(players_pos);
+	}
+	else if (key_rooms_positions.find(players_pos) != -1)
+	{
+		key_rooms_positions.remove(key_rooms_positions.find(players_pos));
+		_special_rooms_loader(disc_rooms_positions, key_rooms_positions, item_rooms_positions, heal_rooms_positions, boss_rooms_positions);
+		key_rooms_positions.push_back(players_pos);
+	}
+	else if (item_rooms_positions.find(players_pos) != -1)
+	{
+		item_rooms_positions.remove(item_rooms_positions.find(players_pos));
+		_special_rooms_loader(disc_rooms_positions, key_rooms_positions, item_rooms_positions, heal_rooms_positions, boss_rooms_positions);
+		item_rooms_positions.push_back(players_pos);
+	}
+	else if (heal_rooms_positions.find(players_pos) != -1)
+	{
+		heal_rooms_positions.remove(heal_rooms_positions.find(players_pos));
+		_special_rooms_loader(disc_rooms_positions, key_rooms_positions, item_rooms_positions, heal_rooms_positions, boss_rooms_positions);
+		heal_rooms_positions.push_back(players_pos);
+	}
+	else if (boss_rooms_positions.find(players_pos) != -1)
+	{
+		boss_rooms_positions.remove(boss_rooms_positions.find(players_pos));
+		_special_rooms_loader(disc_rooms_positions, key_rooms_positions, item_rooms_positions, heal_rooms_positions, boss_rooms_positions);
+		boss_rooms_positions.push_back(players_pos);
+	}
+
+	_load_undisc_rooms(undisc_rooms_positions);
+
+	return;
 }
 
 bool godot::MiniMapController::_is_on_grid(Vector2 pos)
@@ -246,7 +340,7 @@ void godot::MiniMapController::_start_timer()
 	if (!has_node(NodePath(timer->get_name())))
 		add_child(timer);
 
-	timer->set_wait_time(3);
+	timer->set_wait_time(2);
 	timer->start();
 }
 
--- godot_game_STUDIO_project/godot_game_STUDIO_project/MiniMapController.h
+++ godot_game_STUDIO_project/godot_game_STUDIO_project/MiniMapController.h
@@ -14,11 +14,19 @@ namespace godot
 		Ref<PackedScene> curr_room = nullptr;
 		Ref<PackedScene> undisc_room = nullptr;
 		Ref<PackedScene> disc_room = nullptr;
+		Ref<PackedScene> key_room = nullptr;
+		Ref<PackedScene> item_room = nullptr;
+		Ref<PackedScene> heal_room = nullptr;
+		Ref<PackedScene> boss_room = nullptr;
 
 		TextureRect* grid = nullptr;
 
 		Array undisc_rooms_positions = {};
 		Array disc_rooms_positions = {};
+		Array key_rooms_positions = {};
+		Array item_rooms_positions = {};
+		Array heal_rooms_positions = {};
+		Array boss_rooms_positions = {};
 		Vector2 curr_room_position;
 
 		Array keys_positions = {};
@@ -43,18 +51,21 @@ namespace godot
 		void _init();
 		void _ready();
 
-		void _set_positions();
+		
 		bool _load_resources();
+		bool _is_on_grid(Vector2 pos);
+		void _set_positions();
 		void _update_minimap();
-		Vector2 _get_players_pos();
-		Vector2 _normalize_room_pos(Vector2 old_pos);
 		void _clear_map();
-		Array _normalize_all_rooms(Array rooms_pos_array);
-		void _load_disc_rooms(Array  disc_rooms_pos);
+		void _start_treking();
 		void _load_curr_room(Vector2 curr_room_pos);
 		void _load_undisc_rooms(Array  undisc_rooms_pos);
-		bool _is_on_grid(Vector2 pos);
-		void _start_treking();
+		void _load_special_rooms(Array rooms_pos, String type);
+		void _special_rooms_loader(Array dr, Array kr, Array ir, Array hr, Array br);
+		void _update_special_rooms();
+		Vector2 _get_players_pos();
+		Vector2 _normalize_room_pos(Vector2 old_pos);
+		Array _normalize_all_rooms(Array rooms_pos_array);
 
 		void _start_timer();
 		void _on_timeout();
