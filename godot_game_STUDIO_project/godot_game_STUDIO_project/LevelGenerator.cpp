5224dfbebbf6a9716f5edfbcc767c21baecbc78b
pashan27 <pashan27@users.noreply.github.com>
2021-07-14 15:45:44 +0300

    fixed some z-index

--- godot_game_STUDIO_project/godot_game_STUDIO_project/LevelGenerator.cpp
+++ godot_game_STUDIO_project/godot_game_STUDIO_project/LevelGenerator.cpp
@@ -732,6 +732,8 @@ void godot::LevelGenerator::_generate_key(Node2D* room)
 	auto pedestal = cast_to<Node2D>(key_room_pedestal->instance());
 	add_child(pedestal);
 	pedestal->set_global_position(pos);
+	pedestal->set_z_as_relative(false);
+	pedestal->set_z_index(3);
 
 	int key_index = rng->randi_range(0, keys_prefabs_local.size() - 1);
 	Ref<PackedScene> key_prefab = keys_prefabs_local[key_index];
